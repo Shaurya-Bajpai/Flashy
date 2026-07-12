ec578c9904eeb98fa38094e274159cc93d17b5c9
Shaurya Bajpai <shauryabajpai980@gmail.com>
2026-07-12 11:01:53 +0530

    Update Dependencies and Add Staging Environment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,15 +28,31 @@ android {
             applicationIdSuffix = ".dev"
             versionNameSuffix = "-dev"
             resValue("string", "app_name", "Flashy-Dev")
+            buildConfigField("String", "ENVIRONMENT", "\"dev\"")
+            buildConfigField("Boolean", "ENABLE_LOGGING", "true")
+        }
+
+        create("stage") {
+            dimension = "environment"
+            applicationIdSuffix = ".stage"
+            versionNameSuffix = "-stage"
+            resValue("string", "app_name", "Flashy Stage")
+            buildConfigField("String", "ENVIRONMENT", "\"stage\"")
+            buildConfigField("Boolean", "ENABLE_LOGGING", "true")
         }
 
         create("prod") {
             dimension = "environment"
             resValue("string", "app_name", "Flashy")
+            buildConfigField("String", "ENVIRONMENT", "\"prod\"")
+            buildConfigField("Boolean", "ENABLE_LOGGING", "false")
         }
     }
 
     buildTypes {
+        debug {
+            isMinifyEnabled = false
+        }
         release {
             isMinifyEnabled = true
             isShrinkResources = true
@@ -55,6 +71,7 @@ android {
     }
     buildFeatures {
         compose = true
+        buildConfig = true
     }
 }
 
